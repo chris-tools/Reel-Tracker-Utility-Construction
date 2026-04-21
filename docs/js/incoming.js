07616fa922002d9d79fa8c972d8d46161b0b8382
Chris Gagnon <foto2mn@comcast.net>
2026-04-21 09:29:53 -0400

    Update incoming.js

--- docs/js/incoming.js
+++ docs/js/incoming.js
@@ -712,8 +712,8 @@ function updateManualAddState(){
   }
 
 function mmddyyyy(d){
-  const mm = String(d.getMonth() + 1).padStart(2, '0');
-  const dd = String(d.getDate()).padStart(2, '0');
+  const mm = d.getMonth()+1;
+  const dd = d.getDate();
   const yyyy = d.getFullYear();
   return `${mm}/${dd}/${yyyy}`;
 }
