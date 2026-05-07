1b6dc97f8fdfc85ed538c413a6b84ad84cb58f15
Chris Gagnon <foto2mn@comcast.net>
2026-05-07 18:23:45 -0400

    Update return.js to enable copyReturnEmail always
    Remove disabling condition for copyReturnEmail button.

--- docs/js/return.js
+++ docs/js/return.js
@@ -241,8 +241,7 @@ const entryOk =
 
   // Done (Export) is enabled when there's at least 1 entry in the session
   if (returnExport) returnExport.disabled = !(returnSession.length > 0);
-  if (copyReturnEmail) copyReturnEmail.disabled = !(returnSession.length > 0);
-}
+ }
 
   function updateScanUI(){
    if (dismissLastScanned) dismissLastScanned.disabled = !lastScan;
